35135d76ac02bfaa9d84bdf4dc70e52143e8f056
DrDevoo <krichard001@icloud.com>
2023-03-20 21:25:31 +0100

    u

--- index.js
+++ index.js
@@ -13,9 +13,21 @@ import {
     TaxSubject,
     TaxSubjects} from 'szamlazz.js'
     import * as fs from 'fs';
+    import express from "express";
+    const app = express();
+    import * as http from 'http';
+    import * as https from 'https';
+    import * as dotenv from 'dotenv'
+    dotenv.config()
+    import bodyParser from "body-parser";
+    app.use(bodyParser.json());
+app.post("/", async (req, res) => {
+  const order = req.body
+  console.log(order)
 
-const szamlazzClient = new Client({
-    authToken: 'y8tx6fuss3cqxtqmvanbb6yh3pv7u3dddbfxq9wyg6',
+  console.log("!Számla kiállítása folyamatban!")
+  const szamlazzClient = new Client({
+    authToken: process.env.TOKEN,
     eInvoice: true, // create e-invoice. optional, default: false
     requestInvoiceDownload: true, // downloads the issued pdf invoice. optional, default: false
     downloadedInvoiceCount: 1, // optional, default: 1
@@ -24,60 +36,75 @@ const szamlazzClient = new Client({
 })
 let seller = new Seller({ // everyting is optional
     bank: {
-      name: 'Test Bank <name>',
-      accountNumber: '11111111-11111111-11111111'
+      name: 'OTP Bank',
+      accountNumber: '11720049-21456035'
     },
     email: {
-      replyToAddress: 'test@email.com',
-      subject: 'Invoice email subject',
-      message: 'This is an email message'
+      replyToAddress: 'krichard@elenora.hu',
+      subject: 'Új számla kiállítva',
+      message: 'Az új számla:'
     },
     issuerName: ''
 })
 let buyer = new Buyer({
-    name: 'Some Buyer Name ' + Math.random(),
-    country: '',
-    zip: '1234',
-    city: 'City',
-    address: 'Some street address',
-    taxNumber: '12345678-1-42',
+    name: order.u_firstname + " " + order.u_name,
+    country: order.u_legio,
+    zip: order.u_postnumber,
+    city: order.u_city,
+    address: order.u_postnumber +", "+order.u_city+" "+order.u_addresse,
+    taxNumber: '0',
     postAddress: {
-      name: 'Some Buyer Name',
-      zip: '1234',
-      city: 'City',
-      address: 'Some street address'
+      name: order.u_firstname + " " + order.u_name,
+      zip: order.u_postnumber,
+      city: order.u_city,
+      address: order.u_postnumber +", "+order.u_city+" "+order.u_addresse
     },
     issuerName: '',
     identifier: 1,
-    phone: '',
+    phone: order.u_tel,
     comment: ''
 })
 let soldItem1 = new Item({
-    label: 'First item',
-    quantity: 2,
-    unit: 'qt',
-    vat: 27, // can be a number or a special string
-    netUnitPrice: 100.55, // calculates gross and net values from per item net
-    comment: 'Ez egy árvíztűrő tükörfúrógép'
+  label: 'Arany karkötő',
+  quantity: 1,
+  unit: 'db',
+  vat: 0,
+  grossUnitPrice: 4490 // calculates net and total values from per item gross
 })
 let soldItem2 = new Item({
-    label: 'Second item',
-    quantity: 5,
-    unit: 'qt',
-    vat: 27,
-    grossUnitPrice: 1270 // calculates net and total values from per item gross
+    label: 'Jáde karkötő',
+    quantity: 1,
+    unit: 'db',
+    vat: 0,
+    grossUnitPrice: 2900 // calculates net and total values from per item gross
   })
 let invoice = new Invoice({
-    paymentMethod: PaymentMethods.BankTransfer, // optional, default: BankTransfer
-    currency: Currencies.Ft, // optional, default: Ft
-    language: Languages.Hungarian, // optional, default: Hungarian
-    seller: seller, // the seller, required
-    buyer: buyer, // the buyer, required
+    paymentMethod: PaymentMethods.Cash, // optional, default: BankTransfer
+    currency: Currencies.Ft,
+    language: Languages.Hungarian,
+    seller: seller,
+    buyer: buyer,
     items: [ soldItem1, soldItem2 ], // the sold items, required
     prepaymentInvoice: false // prepayment/deposit invoice should be issued, optional, default: false
   })
 const result = await szamlazzClient.issueInvoice(invoice)
 if (result.pdf) {
-    var buffer = Buffer.from(result.pdf, 'base64')
-    fs.writeFileSync('./szamlak/'+result.invoiceId, buffer)
+    res.status(200).json(result)
+    console.log("!!A számla elküldve a gerinc szerver részére!!")
 }
+});
+
+//Szerver certificates
+const httpServer = http.createServer(app);
+//Az app nyitott portjai
+httpServer.listen(4000, () => {
+  console.log('---Számlázó szerver elerhető a 4000 porton---');
+});
+
+const httpsServer = https.createServer({
+  key: fs.readFileSync('/etc/letsencrypt/live/elenora.hu/privkey.pem'),
+  cert: fs.readFileSync('/etc/letsencrypt/live/elenora.hu/fullchain.pem'),
+}, app);
+httpsServer.listen(444, () => {
+  console.log('---HTTPS szerver elerheto a 444 porton---');
+});
